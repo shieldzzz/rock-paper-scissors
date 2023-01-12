b8f93f8c7cd799a724ef017663bea5157489af62
shields <117888554+shieldzzz@users.noreply.github.com>
2023-01-12 15:09:59 -0800

    Update JS from scratch, add intial function to get computer choice.

--- rps.js
+++ rps.js
@@ -1,36 +1,7 @@
-const choices = ['rock', 'paper', 'scissors'];
-
-function game() {
-playRound();
-
-}
-
-function playRound() {
-    const playerSelection = playerChoice();
-    const compSelection = computerChoice();
-}
-
-function playerChoice() {
-    let input = prompt("Type Rock, Paper, Scissors");
-    while (input == null) {
-        input = prompt("Type Rock, Paper, Scissors");
-    }
-    input = input.toLowerCase();
-    let check = validateInput(input);
-    if (check == true) {
-    console.log(input);
-    }
-}
-
-function computerChoice() {
-    return choices[Math.floor(Math.random() * 3)];
-}
-
-function validateInput(choice) {
-    if(choices.includes(choice)) {
-        return true;
-    }
-        return false;    
-}
-
-game ();
+function getComputerChoice() {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  }
+  
+  console.log(getComputerChoice());
