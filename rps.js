4bf97b8d4872bf34f62fb0ec34915f81aaa46ef3
shields <117888554+shieldzzz@users.noreply.github.com>
2023-01-24 13:40:52 -0800

    Remove comments for readability

--- rps.js
+++ rps.js
@@ -1,12 +1,9 @@
-// Function to get the computer selection
 function getComputerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
     let randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
   }  
-//   console.log(getComputerChoice());
 
-// Function that plays single round of game
 function playRound (playerSelection, computerSelection) {
     let result;
     playerSelection = playerSelection.toLowerCase();
@@ -28,11 +25,7 @@ function playRound (playerSelection, computerSelection) {
         return result = 'Please enter Rock, Paper, or Scissors';
     }
 }   
-// const playerSelection = 'rock';
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
 
-// game function that calls playRound inside and plays 5 rounds with a for loop
 function game() {
     let playerScore = 0;
     let computerScore = 0;
