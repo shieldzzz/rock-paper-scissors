e6c099328c7a7c9011b302d8806ed4afa09a9575
shields <117888554+shieldzzz@users.noreply.github.com>
2023-03-01 12:39:13 -0800

    Add divs for result text, player and computer score, declare HTML elements in JS

--- rps.js
+++ rps.js
@@ -1,83 +1,53 @@
-// Game logic
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorsBtn = document.getElementById('scissorsBtn');
+const resultText = document.getElementById('resultText');
+const playerScoreText = document.getElementById('playerScore');
+const computerScoreText = document.getElementById('computerScore');
+
 let playerScore = 0;
 let computerScore = 0;
-let playerSelection = '';
-let roundWinner = '';
 
-const choices = ['rock', 'paper', 'scissors'];
-function getComputerChoice() {
-    let randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-  }
-
-let computerSelection = getComputerChoice();
-
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        roundWinner = 'Tie';
-    }
-    if (
-    (playerSelection === 'rock' && computerSelection === 'scissors') ||
-    (playerSelection === 'paper' && computerSelection === 'rock') ||
-    (playerSelection === 'scissors' && computerSelection === 'paper')
-    ) {
-        roundWinner = 'player';
-    }
-    if (
-    (playerSelection === 'rock' && computerSelection === 'paper') ||
-    (playerSelection === 'paper' && computerSelection === 'scissors') ||
-    (playerSelection === 'scissors' && computerSelection === 'rock')
-    ) {
-        roundWinner = 'computer';
-    }
-    return updateScore();
-}
-
-function updateScore() {
-    playRound(playerSelection, computerSelection);
-    if (roundWinner.includes('player')) {
-        playerScore++;
-    } else if (roundWinner.includes('computer')) {
-        computerScore++;
-    }
-    return {playerScore, computerScore}
-}
-
-
-//  console.log(playRound('rock', getComputerChoice()));
-
-const rockBtn = document.querySelector('#rock');
+// Buttons
 rockBtn.addEventListener('click', () => {
-    playerSelection = 'rock';
+    const playerSelection = 'rock';
+    const computerChoice = getComputerChoice();
+
 });
 
 
 
-const paperBtn = document.querySelector('#paper');
-const scissorsBtn = document.querySelector('#scissors');
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    let randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
 
 
-// function game() {
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     for (let i = 0; i < 5; i++) {
-//         let playerSelection = prompt('Choose Rock, Paper, or Scissors').toLowerCase();
-//         let computerSelection = getComputerChoice();
-//         let roundResult = playRound(playerSelection, computerSelection);
-//         console.log(roundResult);
-//         if (roundResult.includes('Win')) {
-//             playerScore++;
-//         } else if (roundResult.includes('Lose')) {
-//             computerScore++;
-//         }
+
+
+
+
+// function playRound (playerSelection, computerSelection) {
+//     if (playerSelection === computerSelection) {
+//         roundWinner = 'Tie';
 //     }
-//     if (playerScore > computerScore) {
-//         console.log('You win the game! \nPlayer Score: ' + playerScore + ' \nComputer Score: ' + computerScore);
-//     } else if (playerScore < computerScore) {
-//         console.log('You lose the game! \nPlayer Score: ' + playerScore + ' \nComputer Score: ' + computerScore);
-//     } else {
-//         console.log('It\'s a tie game! \nPlayer Score: ' + playerScore + ' \nComputer Score: ' + computerScore);
+//     if (
+//     (playerSelection === 'rock' && computerSelection === 'scissors') ||
+//     (playerSelection === 'paper' && computerSelection === 'rock') ||
+//     (playerSelection === 'scissors' && computerSelection === 'paper')
+//     ) {
+//         roundWinner = 'player';
 //     }
+//     if (
+//     (playerSelection === 'rock' && computerSelection === 'paper') ||
+//     (playerSelection === 'paper' && computerSelection === 'scissors') ||
+//     (playerSelection === 'scissors' && computerSelection === 'rock')
+//     ) {
+//         roundWinner = 'computer';
+//     }
+//     return updateScore();
 // }
 
-// game();
+
